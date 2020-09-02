d798bfb8864ed2ec6a2d7f108ebd41696c624c99
Walter Bryan <wallythewebdev@gmail.com>
2020-09-02 20:38:52 +0100

    Init

---
+++ build/precache-manifest.fdb0bd6c58e19dc9b21a11f482663121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3cba6577e65562b8c62bc9da44dabe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06384d901faccd026769",
+    "url": "/static/css/main.8f02f2db.chunk.css"
+  },
+  {
+    "revision": "537f79d96c98934f1c01",
+    "url": "/static/js/2.1f9a151d.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/static/js/2.1f9a151d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06384d901faccd026769",
+    "url": "/static/js/main.d7b590f4.chunk.js"
+  },
+  {
+    "revision": "b52de76f4307ef2d8caa",
+    "url": "/static/js/runtime-main.d7823b53.js"
+  }
+]);
